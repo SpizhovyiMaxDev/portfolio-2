cb162cd8f0cb83001009b49965323be90c3f0193
SpizhovyiMaxDev <144062499+SpizhovyiMaxDev@users.noreply.github.com>
2023-12-06 00:08:27 -0800

    message

--- dist/index.672d4772.js
+++ dist/index.672d4772.js
@@ -3,9 +3,22 @@ class App {
     btnToggleNav = document.querySelector(".nav-btn");
     constructor(){
         this.btnToggleNav.addEventListener("click", this._toggleNavigation.bind(this));
+        this.nav.addEventListener("click", this._scrollToSection.bind(this));
     }
     _toggleNavigation() {
         this.nav.classList.toggle("show-mobile-nav");
+    }
+    _scrollToSection(e) {
+        e.preventDefault();
+        const link = e.target.closest(".main-nav-list__link");
+        if (!link) return;
+        const id = link.getAttribute("href");
+        this.nav.classList.remove("show-mobile-nav");
+        window.innerWidth > 944 ? document.querySelector(id).scrollIntoView({
+            behavior: "smooth"
+        }) : setTimeout(()=>document.querySelector(id).scrollIntoView({
+                behavior: "smooth"
+            }), 500);
     }
 }
 new App();
